e4427f76240922bf7114e17ad572af0136ca36bc
Mars <marstherobot@users.noreply.github.com>
2018-02-03 23:35:26 +0100

    Added new fonts

--- medicus/settings/production.py
+++ medicus/settings/production.py
@@ -84,12 +84,8 @@ def get_env_variable(var_name: str) -> str:
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': get_env_variable('MEDICUS_DB'),
-        'USER': 'medicus',
-        'PASSWORD': get_env_variable('MEDICUS_PASSWORD'),
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'mydatabase',
     }
 }
 
